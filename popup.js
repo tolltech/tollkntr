a277d93ebdd36151ff77d4a7c742ec0295287100
tolltech <alexandrovpe@gmail.com>
2023-08-14 14:39:05 +0400

    move to server ticks convert

--- popup.js
+++ popup.js
@@ -1,11 +1,34 @@
-$(document).ready(function () {
+$(document).ready(function () {    
     $('#ticks_input').change(function (evt) {
         var v = $('#ticks_input').val();
-        if (isNaN(v)){
-            $('#ticks_label').html(dateTimeToTicks(v));
-        }
-        else{
-            $('#ticks_label').html(ticksToDateTime(v));
-        }
+
+        getKntr('dateticks/' + v, function(responseData, textStatus, jqXHR){
+            $('#ticks_label').html(responseData); 
+        });       
     });
 });
+
+const domainHost = "https://tolltech.ru";
+function getKntr(url, callback){
+    $.ajax({
+        type: 'GET',
+        url: domainHost + '/kntr/' + url,
+        contentType: 'application/json; charset=utf-8',
+        crossDomain: true,
+        // data: JSON.stringify({ Text: JSON.stringify(tracks) }),
+        // dataType: 'json',
+        cache: false,
+        success: callback,
+        complete: function (responseData, textStatus, jqXHR) {
+            //alert('complete');
+        },
+        error: function (responseData, textStatus, errorThrown) {
+            // alert('error');
+            alert('POST failed.\r\n'
+                + JSON.stringify(responseData) + '\r\n'
+                + JSON.stringify(textStatus) + '\r\n'
+                + JSON.stringify(errorThrown) + '\r\n'
+            );
+        }
+    });
+}
