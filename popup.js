9d448277dfb0f4a76e10bdb5bafcaedcf407bc3a
tolltech <alexandrovpe@gmail.com>
2023-08-22 17:55:18 +0400

    selection ticks or date with convert popup

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#ticks_input').change(function (evt) {
         var v = $('#ticks_input').val();
 
-        getKntr('dateticks/' + v, function(responseData, textStatus, jqXHR){
+        getKntr('dateticks/' + v, function(responseData){
             $('#ticks_label').html(responseData); 
         });       
     });
--- triggerDateTime.js
+++ triggerDateTime.js
@@ -3,6 +3,9 @@ document.addEventListener('mouseup', selectionHandlerFunction, false);
 
 // Mouse up event handler function
 function selectionHandlerFunction(event) {
+    if (event.target && event.target.id == "ticks-convert-div") {
+        return;
+    }
 
     if (document.contains(document.getElementById("ticks-convert-snippet"))) {
         document.getElementById("ticks-convert-snippet").remove();
@@ -11,18 +14,28 @@ function selectionHandlerFunction(event) {
     var text = window.getSelection().toString();
     if (text.length == 0) return;
 
-    //todo: validate long or datetime
+    var wasProcessed = false;
+    if (!isNaN(text)
+        || (text.length >= 6 && !isNaN(Date.parse(text))
+        )
+    ) {
+        getKntr('dateticks/' + text, function (responseData) {
+            $('#ticks-convert-div').html(responseData);
+        });
+        wasProcessed = true;
+    }
 
+    if (!wasProcessed) return;
 
     var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        
+
     // Get cursor position
     const posX = event.clientX + 30;
     const posY = event.clientY - 30 + scrollTop;
 
     document.body.insertAdjacentHTML('beforeend', '<div id="ticks-convert-snippet" style="position: absolute; top: '
-    +posY+'px; left: '+posX+'px;"><div>'
-    +'<div style="background:#ffffff; color:#000000; padding: 3px;">Convert</div></div></div>');
+        + posY + 'px; left: ' + posX + 'px;"><div>'
+        + '<div id="ticks-convert-div" style="background:#000000; color:#ffffff; padding: 3px;">Convert</div></div></div>');
 
     //alert(text);
 }
