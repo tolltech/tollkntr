c668bb2e6d27bb623c0f9072c3ca71cdd794287f
tolltech <alexandrovpe@gmail.com>
2024-02-08 16:55:53 +0400

    Random generator

--- triggerHelper.js
+++ triggerHelper.js
@@ -6,6 +6,6 @@ function replaceSelectedText(elem, text) {
     var start = elem.selectionStart;
     var end = elem.selectionEnd;
     elem.value = elem.value.slice(0, start) + text + elem.value.substr(end);
-    elem.selectionStart = start + text.length;
-    elem.selectionEnd = elem.selectionStart;
+    elem.selectionStart = start;
+    elem.selectionEnd = elem.selectionStart + text.length;
 }
---
+++ triggerRandomGenerator.js
@@ -0,0 +1,34 @@
+var randomTextQueue = [];
+var randomTextQueueInd = 0;
+
+$(document).on('keydown', function (e) {
+    var tag = e.target.tagName.toLowerCase();
+    if (tag != 'input' && tag != 'textarea')
+        return;
+
+    if (!e.ctrlKey || !e.altKey || !e.shiftKey)
+        return;
+
+    if (e.which != 39 && e.which != 37)//right & left
+        return;
+
+    if (e.which == 37 && randomTextQueueInd > 0) {//left
+        randomTextQueueInd--;
+    }
+    if (e.which == 39) {//right
+        randomTextQueueInd++;
+    }
+
+    if (randomTextQueue.length <= 0 || randomTextQueueInd >= randomTextQueue.length) {       
+        getKntr('randomtext/dummy', function(data){
+            var msg = data;
+            randomTextQueue.push(msg);
+            randomTextQueueInd = randomTextQueue.length - 1;
+
+            replaceSelectedText(document.activeElement, randomTextQueue[randomTextQueueInd]);
+        });
+    }
+    else {
+        replaceSelectedText(document.activeElement, randomTextQueue[randomTextQueueInd]);
+    }
+});
