89964e15241d7b55a564e9021cab62e484233f41
tolltech <alexandrovpe@gmail.com>
2024-10-03 11:25:17 +0400

    add new selector for autologin

--- triggerLogin.js
+++ triggerLogin.js
@@ -2,4 +2,5 @@ setInterval(function () {
     $('.btn-login-openid').click();
     $('.btn-login-oidc').click();
     $("img[alt='Контур.Паспорт']").click();
+    $("img[alt='Kontur.Passport']").click();
 }, 1000);
